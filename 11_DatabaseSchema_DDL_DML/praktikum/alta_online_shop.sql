4da07b01325aff123a85897f97663b737e551fb8
apocalynha <adittamvan88@gmail.com>
2023-09-10 23:27:37 +0700

    Tugas Soal Database Schema, DDL, DML

--- 11_DatabaseSchema_DDL_DML/praktikum/alta_online_shop.sql
+++ 11_DatabaseSchema_DDL_DML/praktikum/alta_online_shop.sql
@@ -1,12 +1,30 @@
+-- 1. Create database alta_online_shop.
 CREATE DATABASE alta_online_shop;
 
-CREATE TABLE user (
+-- 2. Dari schema Olshop yang telah kamu kerjakan di, Implementasikanlah menjadi table pada MySQL.
+-- 2. a. Create tabel user
+CREATE TABLE users (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   alamat VARCHAR(255),
   dob DATE,
   status SMALLINT,
   gender CHAR(1),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- 2. Create table product, product type, operators, product description, payment_method.
+CREATE TABLE operators (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product_types (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -18,41 +36,28 @@ CREATE TABLE products (
   code VARCHAR(50),
   name VARCHAR(100),
   status SMALLINT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (product_type_id) REFERENCES product_types(id),
   FOREIGN KEY (operator_id) REFERENCES operators(id)
-);
-
-CREATE TABLE product_types (
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
-);
-
-CREATE TABLE operators (
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
 );
 
 CREATE TABLE product_descriptions (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   description TEXT, 
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE payment_methods (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   status SMALLINT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+-- 2. c. Create table transaction, transaction detail.
 CREATE TABLE transactions (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_id INT(11),
@@ -60,8 +65,8 @@ CREATE TABLE transactions (
   status VARCHAR(10),
   total_qty INT(11),
   total_price NUMERIC(25,2),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
 );
@@ -72,8 +77,60 @@ CREATE TABLE transaction_details (
   status VARCHAR(10),
   qty INT(11),
   price NUMERIC(25,2),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (transaction_id) REFERENCES transactions(id),
   FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+-- 3. Create tabel kurir dengan field id, name, created_at, updated_at.
+CREATE TABLE kurir (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255), 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- 4. Tambahkan ongkos_dasar column di tabel kurir.
+ALTER TABLE kurir ADD ongkos_dasar INT;
+
+-- 5. Rename tabel kurir menjadi shipping.
+ALTER TABLE kurir RENAME TO shipping;
+
+-- 6. Hapus / Drop tabel shipping karena ternyata tidak dibutuhkan.
+DROP TABLE shipping;
+
+-- 7. Silahkan menambahkan entity baru dengan relation 1-to-1, 1-to-many, many-to-many. Seperti:
+-- 7. a. 1-to-1: payment method description.
+CREATE TABLE payment_method_description (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    payment_method_id INT,
+    description TEXT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
+);
+
+-- 7. b. 1-to-many: user dengan alamat.
+CREATE TABLE address (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    street_address TEXT,
+    city VARCHAR(255),
+    province VARCHAR(255),
+    country VARCHAR(255),
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- 7. c. many-to-many: user dengan payment method menjadi user_payment_method_detail.
+CREATE TABLE user_payment_method_detail (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    payment_method_id INT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
 );
--- 11_DatabaseSchema_DDL_DML/praktikum/readme.md
+++ 11_DatabaseSchema_DDL_DML/praktikum/readme.md
@@ -10,4 +10,39 @@ Berikut link draw.io : https://drive.google.com/file/d/1TPCH9P5Xu2KDDmtkbeQIl6m_
 
 ![skema_db](/11_DatabaseSchema_DDL_DML/screenshots/skema_db.png)
 
-## 
+## Data Definition Language (DDL)
+
+1. Create database alta_online_shop.
+![create_db](/11_DatabaseSchema_DDL_DML/screenshots/create_db.JPG)
+
+2. Dari schema Olshop yang telah kamu kerjakan di, Implementasikanlah menjadi table pada MySQL.
+    a. Create table user.
+![create_2a](/11_DatabaseSchema_DDL_DML/screenshots/create_2a.JPG)
+
+    b. Create table product, product type, operators, product description, payment_method.
+![create_2b](/11_DatabaseSchema_DDL_DML/screenshots/create_2b.JPG)
+
+    c. Create table transaction, transaction detail.
+![create_2c](/11_DatabaseSchema_DDL_DML/screenshots/create_2c.JPG)
+
+3. Create tabel kurir dengan field id, name, created_at, updated_at.
+![create_3](/11_DatabaseSchema_DDL_DML/screenshots/create_3.JPG)
+
+4. Tambahkan ongkos_dasar column di tabel kurir.
+![create_4](/11_DatabaseSchema_DDL_DML/screenshots/create_4.JPG)
+
+5. Rename tabel kurir menjadi shipping.
+![create_5](/11_DatabaseSchema_DDL_DML/screenshots/create_5.JPG)
+
+6. Hapus / Drop tabel shipping karena ternyata tidak dibutuhkan.
+![create_6](/11_DatabaseSchema_DDL_DML/screenshots/create_6.JPG)
+
+7. Silahkan menambahkan entity baru dengan relation 1-to-1, 1-to-many, many-to-many. Seperti:
+    a. 1-to-1: payment method description.
+![create_7a](/11_DatabaseSchema_DDL_DML/screenshots/create_7a.JPG)
+
+    b. 1-to-many: user dengan alamat.
+![create_7b](/11_DatabaseSchema_DDL_DML/screenshots/create_7b.JPG)    
+    
+    c. many-to-many: user dengan payment method menjadi user_payment_method_detail.
+![create_7c](/11_DatabaseSchema_DDL_DML/screenshots/create_7c.JPG)
